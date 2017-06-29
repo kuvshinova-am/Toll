b932597121990d30d993562946f6cf9706cf9ded
kuvshinova-am <leilani4551@gmail.com>
2017-06-29 18:56:35 +0700

    dependency injection

--- tracker-core/src/main/java/jdev/tracker/core/services/DataPeekService.java
+++ tracker-core/src/main/java/jdev/tracker/core/services/DataPeekService.java
@@ -1,7 +1,11 @@
 package jdev.tracker.core.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataPeekService {
+
+    @Autowired
+    private DataSendService dataSendService;
 }
