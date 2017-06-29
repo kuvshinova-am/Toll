b680d4f6d70ed7d83ec80a20104095d5f6b524ac
kuvshinova-am <leilani4551@gmail.com>
2017-06-29 19:00:23 +0700

    configuration bean+postConstruct

--- tracker-core/src/main/java/jdev/tracker/core/services/DataPeekService.java
+++ tracker-core/src/main/java/jdev/tracker/core/services/DataPeekService.java
@@ -3,9 +3,16 @@ package jdev.tracker.core.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class DataPeekService {
 
     @Autowired
     private DataSendService dataSendService;
+
+    @PostConstruct
+    private void init() {
+        dataSendService.callFromInit();
+    }
 }
--- tracker-core/src/main/java/jdev/tracker/core/services/InjectionContext.java
+++ tracker-core/src/main/java/jdev/tracker/core/services/InjectionContext.java
@@ -1,7 +1,18 @@
 package jdev.tracker.core.services;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InjectionContext {
+
+    @Bean
+    public DataPeekService peekService() {
+        return new DataPeekService();
+    }
+
+    @Bean
+    public DataSendService sendService() {
+        return new DataSendService();
+    }
 }
