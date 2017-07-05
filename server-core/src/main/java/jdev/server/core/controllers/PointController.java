48a14d95b7149e30075da0c0afdc48fc20df5ff3
kuvshinova-am <leilani4551@gmail.com>
2017-07-05 17:34:37 +0700

    add POST function in PointController

--- server-core/src/main/java/jdev/server/core/controllers/PointController.java
+++ server-core/src/main/java/jdev/server/core/controllers/PointController.java
@@ -1,8 +1,33 @@
 package jdev.server.core.controllers;
 
-import org.springframework.stereotype.Controller;
+import jdev.dto.Point;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.FileWriter;
+import java.io.IOException;
+
+@RestController
+@RequestMapping(path = "/point")
 public class PointController {
 
+    private static final Logger log = LoggerFactory.getLogger(PointController.class);
+
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    boolean receive(@RequestBody Point point) {
+        log.info(String.valueOf(point));
+
+        try(FileWriter writer = new FileWriter("log.txt", false)) {
+            writer.write(String.valueOf(point));
+            writer.flush();
+        }
+        catch(IOException ex) {
+            log.info(ex.getMessage());
+        }
+        return true;
+    }
 }
--- tracker-core/src/main/java/jdev/tracker/core/services/DataSendService.java
+++ tracker-core/src/main/java/jdev/tracker/core/services/DataSendService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-
-
 @Service
 public class DataSendService {
 
