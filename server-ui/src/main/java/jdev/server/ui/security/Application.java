869e1ead5bb6f19284add3bdc8eb8e95354decdb
kuvshinova-am <leilani4551@gmail.com>
2017-07-24 17:35:31 +0700

    added spring web security  in server-ui

--- server-ui/src/main/java/jdev/server/ui/security/Application.java
+++ server-ui/src/main/java/jdev/server/ui/security/Application.java
@@ -1,9 +1,11 @@
-package jdev.server.ui;
+package jdev.server.ui.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("jdev.server.ui.config")
 public class Application {
     public static void main(String[] args) throws Throwable {
         SpringApplication.run(Application.class, args);
