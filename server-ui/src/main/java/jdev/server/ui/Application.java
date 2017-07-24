cdc6a8b497bcd14ae5c842305b2abfb2024a8df2
kuvshinova-am <leilani4551@gmail.com>
2017-07-24 14:45:56 +0700

    added spring boot application in server-ui

---
+++ server-ui/src/main/java/jdev/server/ui/Application.java
@@ -0,0 +1,12 @@
+package jdev.server.ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(Application.class, args);
+
+    }
+}
